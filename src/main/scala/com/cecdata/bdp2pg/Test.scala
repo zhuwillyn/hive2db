ea4f00088a1b11edc68ab3d3dbb5598569ba71a4
zhuweilin <zhuweilin@cecdat.com>
2018-07-04 14:23:00 +0800

    初始化项目结构

---
+++ src/main/scala/com/cecdata/bdp2pg/Test.scala
@@ -0,0 +1,13 @@
+package com.cecdata.bdp2pg
+
+/**
+  * @project bdp2pg
+  * @description
+  * @author zhuweilin
+  * @mail zhuwillyn@163.com
+  * @date 2018/07/04 14:22
+  * @version
+  */
+class Test {
+
+}
